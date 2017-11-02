25987219368daf645fadb146325bfa37ec641f22
Oleg Timofeev <oleg.timofeev20@gmail.com>
2017-11-02 22:27:59 +0200

    02.11

--- lib/src/json_object.dart
+++ lib/src/json_object.dart
@@ -1,33 +1,36 @@
 import 'dart:convert';
 import 'dart:mirrors';
 
-abstract class JsonObject{
+abstract class JsonObject {
+  JsonObject() {}
 
-    JsonObject();
+  JsonObject.parseString(String target) {
+    fromJsonString(target);
+  }
 
-    JsonObject.parseString(String target){
-        fromJsonString(target);
-    }
+  JsonObject.parse(Map target) {
+    fromJson(target);
+  }
 
-    JsonObject.parse(Map target){
-        fromJson(target);
-    }
+  Map toJson() {
+    Map map = new Map();
+    InstanceMirror im = reflect(this);
+    ClassMirror cm = im.type;
+    var decls = cm.declarations.values.where((dm) => dm is VariableMirror);
+    decls.forEach((dm) {
+      var key = MirrorSystem.getName(dm.simpleName);
+      var val = im.getField(dm.simpleName).reflectee;
+      map[key] = val;
+    });
 
-    Map toJson() { 
-        Map map = new Map();
-        InstanceMirror im = reflect(this);
-        ClassMirror cm = im.type;
-        var decls = cm.declarations.values.where((dm) => dm is VariableMirror);
-        decls.forEach((dm) {
-            var key = MirrorSystem.getName(dm.simpleName);
-            var val = im.getField(dm.simpleName).reflectee;
-            map[key] = val;
-        });
+    return map;
+  }
 
-        return map;
-    }
+  void fromJsonString(String target) => fromJson(JSON.decode(target));
+  void fromJson(Map target);
 
-    void fromJsonString(String target) => fromJson(JSON.decode(target));
-    void fromJson(Map target);
-
+  static List parseList<T extends JsonObject>(String json) {
+    var parsed = JSON.decode(json);
+    return parsed.map((x) => (T as JsonObject).fromJson(x)).toList();
+  }
 }
--- lib/src/json_package.dart
+++ lib/src/json_package.dart
@@ -1,34 +1,40 @@
 import 'json_object.dart';
 import "dart:mirrors";
 
-class JsonPackage<T extends JsonObject> extends JsonObject{
-    List<T> inner;
-    int _time;
-    int _expired = -1;
+class JsonPackage<T extends JsonObject> extends JsonObject {
+  List<T> inner;
+  int _time;
+  int _expired = -1;
 
-    DateTime get time => _time.isNaN ? null : new DateTime.fromMicrosecondsSinceEpoch(_time);
-    set time(DateTime t) { _time = t.millisecondsSinceEpoch; }
+  DateTime get time => _time.isNaN ? null : new DateTime.fromMicrosecondsSinceEpoch(_time);
+  set time(DateTime t) {
+    _time = t.millisecondsSinceEpoch;
+  }
 
-    DateTime get expired => _expired < 0 ? null : new DateTime.fromMicrosecondsSinceEpoch(_expired);
-    set expired(DateTime t) { _expired = t.millisecondsSinceEpoch; }
+  DateTime get expired => _expired < 0 ? null : new DateTime.fromMicrosecondsSinceEpoch(_expired);
+  set expired(DateTime t) {
+    _expired = t.millisecondsSinceEpoch;
+  }
 
-    bool get isExpired => _expired < 1 ? false : new DateTime.now().millisecondsSinceEpoch >= expired.millisecondsSinceEpoch; 
+  bool get isExpired => _expired < 1 ? false : new DateTime.now().millisecondsSinceEpoch >= expired.millisecondsSinceEpoch;
 
-    JsonPackage(List<T> inner, [DateTime time, DateTime expired])
-    {
-        this.inner = inner;
-        time == null ? this.time = new DateTime.now() : this.time = time; 
+  JsonPackage(this.inner, [DateTime expiredIn, DateTime timeIn]) {
+    timeIn == null ? this.time = new DateTime.now() : this.time = timeIn;
+    expired = expiredIn;
+
+    if (expiredIn != null && expiredIn.millisecondsSinceEpoch < timeIn.millisecondsSinceEpoch) {
+      throw new FormatException('Expired time can not be less then time');
     }
+  }
 
-    @override
-    void fromJson(target) {
-        _time = target['_time'];
-        inner = [];
-        target['inner'].forEach((i) {
-            ClassMirror c = reflectClass(T);
-            InstanceMirror im = c.newInstance(const Symbol(''), []);
-            inner.add(im.reflectee..fromJson(i));
-        });
-    }
-
+  @override
+  void fromJson(target) {
+    _time = target['_time'];
+    inner = [];
+    target['inner'].forEach((i) {
+      ClassMirror c = reflectClass(T);
+      InstanceMirror im = c.newInstance(const Symbol(''), []);
+      inner.add(im.reflectee..fromJson(i));
+    });
+  }
 }
